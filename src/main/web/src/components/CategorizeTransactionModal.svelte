58ca51045fc120f743093edee024eef8f1dc982c
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2023-10-18 00:34:18 +0100

    Milestone: Copy SPA build files in Spring Boot JAR

---
+++ src/main/web/src/components/CategorizeTransactionModal.svelte
@@ -0,0 +1,45 @@
+<script>
+
+    import { Alert } from 'flowbite-svelte';
+    import { AccordionItem, Accordion } from 'flowbite-svelte';
+    let spanClass = 'flex-1 ml-3 whitespace-nowrap';
+    import { Tabs, TabItem } from 'flowbite-svelte';
+
+
+</script>
+
+
+<Tabs>
+    <TabItem open title="Profile">
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Profile:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+    <TabItem title="Settings">
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Settings:</b>
+        Looking at the code, I think we can optimize for invoices
+      </p>
+    </TabItem>
+    <TabItem title="Users">
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Users:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+    <TabItem title="Dashboard">
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Dashboard:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+    <TabItem disabled>
+      <span slot="title" class="text-gray-400 dark:text-gray-500">Disabled</span>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Disabled:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+  </Tabs>
+  
---
+++ src/main/web/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import CategorizeTransactionModal from "../components/CategorizeTransactionModal.svelte";
+</script>
+
+<div class="flex-row flex font-mono tracking-tight">
+
+   <section >
+      <CategorizeTransactionModal/>
+   </section>
+
+</div>
